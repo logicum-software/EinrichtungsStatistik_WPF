843e1a2d293301e92de50a0f35c8a37630558e16
buddy <buddy@DESKTOP-P2JA1DG>
2018-04-28 18:29:43 +0200

    First Edition

---
+++ EinrichtungsStatistik/EinrichtungsStatistik/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EinrichtungsStatistik
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBeenden_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Möchten Sie das Programm beenden?", "Programm beenden", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                return;
+            else
+                Application.Current.Shutdown();
+        }
+    }
+}
