09948d5e25dbfae282979a3db03ffd5d96e674fd
buddy <buddy@DESKTOP-P2JA1DG>
2018-04-28 18:51:17 +0200

    Fragebogen und AppData erstellt.

---
+++ EinrichtungsStatistik/EinrichtungsStatistik/AppData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinrichtungsStatistik
+{
+    class AppData
+    {
+        private ArrayList Frageboegen;
+        private ArrayList Fragen;
+
+        public AppData()
+        {
+            Frageboegen = new ArrayList();
+            Fragen = new ArrayList();
+        }
+
+        public AppData(ArrayList frageboegen, ArrayList fragen)
+        {
+            Frageboegen = frageboegen;
+            Fragen = fragen;
+        }
+    }
+}
---
+++ EinrichtungsStatistik/EinrichtungsStatistik/Fragebogen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinrichtungsStatistik
+{
+    class Fragebogen
+    {
+        private String strName;
+        private ArrayList Fragen;
+
+        public Fragebogen()
+        {
+            strName = "";
+            Fragen = new ArrayList();
+        }
+
+        public Fragebogen(String name, ArrayList fragen)
+        {
+            strName = name;
+            Fragen = new ArrayList(fragen);
+        }
+
+        internal String getName()
+        {
+            return strName;
+        }
+
+        internal void setName(String name)
+        {
+            strName = name;
+        }
+
+        internal ArrayList getFragen()
+        {
+            return Fragen;
+        }
+
+        internal void setFragen(ArrayList fragen)
+        {
+            Fragen = fragen;
+        }
+    }
+}
