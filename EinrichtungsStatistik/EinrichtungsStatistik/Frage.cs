35c7f23aaa9b08e57652a9a252ae65cf21b797b7
buddy <buddy@DESKTOP-P2JA1DG>
2018-04-28 18:37:46 +0200

    Class Frage implemented.

---
+++ EinrichtungsStatistik/EinrichtungsStatistik/Frage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinrichtungsStatistik
+{
+    class Frage
+    {
+        private String strFragetext;
+        private int nAntwortart;
+
+        public Frage()
+        {
+            strFragetext = "";
+            nAntwortart = 0;
+        }
+
+        public Frage(String fragetext, int antwortart)
+        {
+            strFragetext = fragetext;
+            nAntwortart = antwortart;
+        }
+
+        internal String getFragetext()
+        {
+            return strFragetext;
+        }
+
+        internal void setFragetext(String fragetext)
+        {
+            strFragetext = fragetext;
+        }
+
+        internal int getAntwortart()
+        {
+            return nAntwortart;
+        }
+
+        internal void setAntwortart(int antwortart)
+        {
+            nAntwortart = antwortart;
+        }
+    }
+}
